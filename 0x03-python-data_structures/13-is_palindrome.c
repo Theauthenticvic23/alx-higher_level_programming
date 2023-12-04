cf71c88bea10b763916279b7da0ea34bc8fd32fe
Theauthenticvic23 <victoradesina345@gmail.com>
2023-12-04 02:28:11 -0800

    palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,57 +1,68 @@
 #include "lists.h"
-#include <stdlib.h>
-#include <stdio.h>
 
 /**
- * add_nodeint - adds a new node
- * @head: head
- * @n: int to add
+ * reverses_listint - reverse
+ * @head: pointer
  * Return: nothing
  */
 
-listint_t *add_nodeint(listint_t **head, const int n)
+void reverse_listint(listint_t **head)
 {
-	listint_t *new;
+	listint_t *prev = NULL;
+	listint_t *current = *head;
+	listint_t *next = NULL;
 
-	new = malloc(sizeof(listint_t));
-
-	if (new == NULL)
-		return (NULL);
-	new->n = n;
-	new->next = *head;
-	*head = new;
-	return (new);
+	while (current)
+	{
+		next = current->next;
+		current->next = prev;
+		prev = current;
+		current = next;
+	}
+	*head = prev;
 }
 /**
- * is_palindrome - identify if a singly linked list is palindrome
- * @head: head
+ * is_palindrome - checks if a linked list is a palindrome
+ * @head: double pointer
  * Return: nothing
  */
 
 int is_palindrome(listint_t **head)
 {
-	listint_t *head2 = *head;
+	listint_t *slow = *head, *fast = *head, *temp = *head, *dup = NULL;
 
-	listint_t *aux = NULL, *aux2 = NULL;
-
-	if (*head == NULL || head2->next == NULL)
+	if (*head == NULL || (*head)->next == NULL)
 		return (1);
-	while (head2 != NULL)
+
+	while (1)
 	{
-		add_nodeint(&aux, head2->n);
-		head2 = head2->next;
-	}
-	aux2 = aux;
-	while (*head != NULL)
-	{
-		if ((*head)->n != aux2->n)
+		fast = fast->next->next;
+		if (!fast)
 		{
-			free_listint(aux);
-			return (0);
+			dup = slow->next;
+			break;
 		}
-		*head = (*head)->next;
-		aux2 = aux2->next;
+		if (!fast->next)
+		{
+			dup = slow->next->next;
+			break;
+		}
+		slow = slow->next;
 	}
-	free_listint(aux);
-	return (1);
+	reverse_listint(&dup);
+
+	while (dup && temp)
+	{
+		if (temp->n == dup->n)
+		{
+			dup = dup->next;
+			temp = temp->next;
+		}
+		else
+			return (0);
+	}
+	if (!dup)
+		return (1);
+
+	return (0);
 }
